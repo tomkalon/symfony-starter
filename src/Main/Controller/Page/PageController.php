0118333171a2b0ddb762a64483245e033dfbee5d
Tomasz Kaliński <tomasz@infosun.pl>
2023-11-09 20:19:18 +0100

    migrations / controller

--- src/Main/Controller/Page/PageController.php
+++ src/Main/Controller/Page/PageController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Main\Page\Controller;
+namespace App\Main\Controller\Page;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
